3a052a1ce88970986de75e2500d360a86635ca22
JV <126692+vilosource@users.noreply.github.com>
2025-05-28 17:20:49 +0300

    Add authentication base template, redesign login and new logout

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.contrib.auth.views import LogoutView
-from .views import CieloLoginView, CieloPasswordChangeView
+from .views import CieloLoginView, CieloLogoutView, CieloPasswordChangeView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', CieloLoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
+    path('logout/', CieloLogoutView.as_view(), name='logout'),
     path('change-password/', CieloPasswordChangeView.as_view(), name='change_password'),
 ]
